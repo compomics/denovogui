5ace85a5f3100669a2ac170da0ebb34693285d20
harald.barsnes <harald.barsnes@d7456372-3d09-dc9c-c65b-cffa522d2526>
2013-03-11 17:22:17 +0000

    Started the job of refactoring the GUI to work/look more like SearchGUI.

--- src/main/java/com/compomics/denovogui/DeNovoSearchHandler.java
+++ src/main/java/com/compomics/denovogui/DeNovoSearchHandler.java
@@ -1,21 +1,15 @@
 package com.compomics.denovogui;
 
-import com.compomics.denovogui.execution.Job;
 import com.compomics.denovogui.execution.jobs.PepnovoJob;
-import com.compomics.denovogui.gui.DeNovoGUI;
 import com.compomics.denovogui.io.FileProcessor;
 import com.compomics.denovogui.io.ModificationFile;
 import com.compomics.util.experiment.identification.SearchParameters;
 import com.compomics.util.experiment.massspectrometry.SpectrumFactory;
 import com.compomics.util.gui.waiting.WaitingHandler;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.Deque;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -30,15 +24,19 @@ import java.util.logging.Logger;
  *
  * @author Marc Vaudel
  * @author Thilo Muth
+ * @author Harald Barsnes
  */
 public class DeNovoSearchHandler {
 
     /**
-     * The pepnovo folder.
+     * The PepNovo folder.
      */
     private File pepNovoFolder;
-    
-    
+    /**
+     * If true, debug output will be given.
+     */
+    private boolean debug = false;
+
     /**
      * Constructor.
      *
@@ -57,7 +55,7 @@ public class DeNovoSearchHandler {
      * @param waitingHandler the waiting handler
      */
     public void startSearch(List<File> spectrumFiles, SearchParameters searchParameters, File outputFolder, WaitingHandler waitingHandler) {
-        
+
         long startTime = System.nanoTime();
         SpectrumFactory spectrumFactory = SpectrumFactory.getInstance();
         waitingHandler.setMaxProgressValue(spectrumFactory.getNSpectra());
@@ -70,51 +68,63 @@ public class DeNovoSearchHandler {
             waitingHandler.appendReport("An error occurred while writing the modification file.", true, true);
             e.printStackTrace();
         }
-        
+
         // Get the number of available cores
         int nCores = Runtime.getRuntime().availableProcessors();
-        System.out.println("number of cores:" + nCores);
-        
+
+        if (debug) {
+            System.out.println("number of cores:" + nCores);
+        }
+
         // Start a fixed thread pool
         ExecutorService threadExecutor = Executors.newFixedThreadPool(nCores);
-        
+
         // Job queue.
         Deque<PepnovoJob> jobs = new ArrayDeque<PepnovoJob>();
         try {
             int nSpectra = FileProcessor.getNumberOfSpectra(spectrumFiles);
-            System.out.println("no. spectra: " + nSpectra);
-            
+
+            if (debug) {
+                System.out.println("no. spectra: " + nSpectra);
+            }
+
             int chunkSize = nSpectra / nCores;
-            System.out.println("chunk size: " + chunkSize);
+
+            if (debug) {
+                System.out.println("chunk size: " + chunkSize);
+            }
+
             List<File> chunkFiles = FileProcessor.chunkFiles(spectrumFiles, chunkSize);
-            
+
             // Distribute the chunked spectra to the different jobs.
             for (File spectrumFile : chunkFiles) {
                 PepnovoJob job = new PepnovoJob(pepNovoFolder, spectrumFile, outputFolder, searchParameters, waitingHandler);
                 jobs.add(job);
-            }       
+            }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(DeNovoSearchHandler.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(DeNovoSearchHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
-     
-        
+
+
         // Execute the jobs from the queue.
-        for (PepnovoJob job : jobs){
+        for (PepnovoJob job : jobs) {
             threadExecutor.execute(job);
         }
-        
+
         // Wait for executor service to shutdown (necessary for reliable exectime calculation.
         try {
-            
             threadExecutor.shutdown();
             threadExecutor.awaitTermination(1, TimeUnit.DAYS);
         } catch (InterruptedException ex) {
             Logger.getLogger(DeNovoSearchHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         double elapsedTime = (System.nanoTime() - startTime) * 1.0e-9;
-        System.out.println("used time (sec): " + elapsedTime);
+
+        if (debug) {
+            System.out.println("used time (sec): " + elapsedTime);
+        }
     }
 }
