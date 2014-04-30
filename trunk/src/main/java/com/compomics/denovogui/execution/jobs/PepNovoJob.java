7931140770f2669e9e27cdb92654db2125ffa29e
harald.barsnes <harald.barsnes@d7456372-3d09-dc9c-c65b-cffa522d2526>
2014-04-30 09:55:46 +0000

    If either the PepNovo or DirecTag commands fails the whole process is now canceled. (Instead of opening empty results as before.) PepNovo is now disabled on Linux 32 bit. DirecTag is now disabled on Mac.

--- trunk/src/main/java/com/compomics/denovogui/execution/jobs/PepNovoJob.java
+++ trunk/src/main/java/com/compomics/denovogui/execution/jobs/PepNovoJob.java
@@ -11,8 +11,9 @@ import java.io.File;
 import java.util.ArrayList;
 
 /**
- * <b>PepNovoJob</b> <p> This job class runs the PepNovo+ software in wrapper
- * mode. </p>
+ * <b>PepNovoJob</b>
+ * <p>
+ * This job class runs the PepNovo+ software in wrapper mode. </p>
  *
  * @author Thilo Muth
  */
@@ -63,81 +64,89 @@ public class PepNovoJob extends Job {
      * Initializes the job, setting up the commands for the ProcessBuilder.
      */
     private void initJob() {
-        
-        // get the DirecTag specific parameters
-        PepnovoParameters pepNovoParameters = (PepnovoParameters) searchParameters.getIdentificationAlgorithmParameter(Advocate.pepnovo.getIndex());
 
-        // full path to executable
-        procCommands.add(pepNovoFolder.getAbsolutePath() + File.separator + exeTitle);
-        procCommands.trimToSize();
+        try {
 
-        // Link to the MGF file
-        procCommands.add("-file");
-        procCommands.add(spectrumFile.getAbsolutePath());
+            // get the PepNovo specific parameters
+            PepnovoParameters pepNovoParameters = (PepnovoParameters) searchParameters.getIdentificationAlgorithmParameter(Advocate.pepnovo.getIndex());
 
-        // Add Model
-        procCommands.add("-model");
-        procCommands.add(pepNovoParameters.getFragmentationModel());
+            // full path to executable
+            procCommands.add(pepNovoFolder.getAbsolutePath() + File.separator + exeTitle);
+            procCommands.trimToSize();
 
-        // Add modifications
-        ArrayList<String> modifications = searchParameters.getModificationProfile().getAllModifications();
-        if (!modifications.isEmpty()) {
-            procCommands.add("-PTMs");
-            procCommands.add(ModificationFile.getModsString(searchParameters.getModificationProfile().getAllModifications()));
+            // Link to the MGF file
+            procCommands.add("-file");
+            procCommands.add(spectrumFile.getAbsolutePath());
+
+            // Add Model
+            procCommands.add("-model");
+            procCommands.add(pepNovoParameters.getFragmentationModel());
+
+            // Add modifications
+            ArrayList<String> modifications = searchParameters.getModificationProfile().getAllModifications();
+            if (!modifications.isEmpty()) {
+                procCommands.add("-PTMs");
+                procCommands.add(ModificationFile.getModsString(searchParameters.getModificationProfile().getAllModifications()));
+            }
+
+            // Add fragment tolerance
+            procCommands.add("-fragment_tolerance");
+            procCommands.add(String.valueOf(searchParameters.getFragmentIonAccuracy()));
+
+            // Add solution number
+            procCommands.add("-num_solutions");
+            procCommands.add(String.valueOf(pepNovoParameters.getHitListLength()));
+
+            // Precursor tolerance
+            procCommands.add("-pm_tolerance");
+            procCommands.add(String.valueOf(searchParameters.getPrecursorAccuracyDalton()));
+
+            // Use spectrum charge: no by default
+            if (!pepNovoParameters.isEstimateCharge()) {
+                procCommands.add("-use_spectrum_charge");
+            }
+
+            // Use spectrum mz: no by default
+            if (!pepNovoParameters.isCorrectPrecursorMass()) {
+                procCommands.add("-use_spectrum_mz");
+            }
+
+            // Remove low quality spectra: yes by default
+            if (!pepNovoParameters.getDiscardLowQualitySpectra()) {
+                procCommands.add("-no_quality_filter");
+            }
+
+            // Generate blast query
+            if (pepNovoParameters.generateQuery()) {
+                procCommands.add("-msb_generate_query");
+                procCommands.add("-msb_query_name");
+                procCommands.add(outputFolder.getAbsolutePath() + System.getProperty("file.separator") + spectrumFile.getName() + ".query");
+            }
+
+            // Add output path
+            outputFile = FileProcessor.getOutFile(outputFolder, spectrumFile);
+            procCommands.trimToSize();
+
+            // print the command to the log file
+            System.out.println(System.getProperty("line.separator") + System.getProperty("line.separator") + "PepNovo+ command: ");
+            for (int i = 0; i < procCommands.size(); i++) {
+                System.out.print(procCommands.get(i) + " ");
+            }
+            System.out.println(System.getProperty("line.separator"));
+
+            // Set the description - yet not used
+            setDescription("PepNovo+");
+            procBuilder = new ProcessBuilder(procCommands);
+            procBuilder.directory(pepNovoFolder);
+
+            // set error out and std out to same stream
+            procBuilder.redirectErrorStream(true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            waitingHandler.appendReport("An error occured running PepNovo+. See error log for details. " + e.getMessage(), true, true);
+            waitingHandler.setRunCanceled();
         }
-
-        // Add fragment tolerance
-        procCommands.add("-fragment_tolerance");
-        procCommands.add(String.valueOf(searchParameters.getFragmentIonAccuracy()));
-
-        // Add solution number
-        procCommands.add("-num_solutions");
-        procCommands.add(String.valueOf(pepNovoParameters.getHitListLength()));
-
-        // Precursor tolerance
-        procCommands.add("-pm_tolerance");
-        procCommands.add(String.valueOf(searchParameters.getPrecursorAccuracyDalton()));
-
-        // Use spectrum charge: no by default
-        if (!pepNovoParameters.isEstimateCharge()) {
-            procCommands.add("-use_spectrum_charge");
-        }
-
-        // Use spectrum mz: no by default
-        if (!pepNovoParameters.isCorrectPrecursorMass()) {
-            procCommands.add("-use_spectrum_mz");
-        }
-
-        // Remove low quality spectra: yes by default
-        if (!pepNovoParameters.getDiscardLowQualitySpectra()) {
-            procCommands.add("-no_quality_filter");
-        }
-
-        // Generate blast query
-        if (pepNovoParameters.generateQuery()) {
-            procCommands.add("-msb_generate_query");
-            procCommands.add("-msb_query_name");
-            procCommands.add(outputFolder.getAbsolutePath() + System.getProperty("file.separator") + spectrumFile.getName() + ".query");
-        }
-
-        // Add output path
-        outputFile = FileProcessor.getOutFile(outputFolder, spectrumFile);
-        procCommands.trimToSize();
-        
-        // print the command to the log file
-        System.out.println(System.getProperty("line.separator") + System.getProperty("line.separator") + "PepNovo+ command: ");
-        for (int i = 0; i < procCommands.size(); i++) {
-            System.out.print(procCommands.get(i) + " ");
-        }
-        System.out.println(System.getProperty("line.separator"));
-
-        // Set the description - yet not used
-        setDescription("PepNovo+");
-        procBuilder = new ProcessBuilder(procCommands);
-        procBuilder.directory(pepNovoFolder);
-
-        // set error out and std out to same stream
-        procBuilder.redirectErrorStream(true);
     }
 
     /**
