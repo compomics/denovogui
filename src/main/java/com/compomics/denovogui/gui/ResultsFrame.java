2dc98bff4d9d65eb967055757548fd1354b461f4
Marc Vaudel <mvaudel@gmail.com>
2015-10-15 12:53:18 +0200

    Updates the peptide export to work with the new back-end structure used for Novor results.

--- src/main/java/com/compomics/denovogui/gui/ResultsFrame.java
+++ src/main/java/com/compomics/denovogui/gui/ResultsFrame.java
@@ -2044,6 +2044,12 @@ public class ResultsFrame extends javax.swing.JFrame {
                                                 assumptions.add(peptideAssumption);
                                             }
                                         }
+                                    } else if (assumption instanceof PeptideAssumption) {
+                                                if (!peptideFound) {
+                                                    peptideFound = true;
+                                                }
+                                    } else {
+                                        throw new UnsupportedOperationException("Peptide mapping not supported for assumption of type " + assumption.getClass() + ".");
                                     }
                                 }
                             }
